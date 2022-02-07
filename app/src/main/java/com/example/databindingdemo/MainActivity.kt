a3451e69d75c287bfe954e3bcda3bffc95f695ee
AndrewWan0927 <andrewwannyx@gmail.com>
2022-02-07 09:50:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/databindingdemo/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.databindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val s:Student = Student("W123456","Ali")
+        binding.student = s
+
+        binding.buttonUpdate.setOnClickListener(){
+
+            s.name = "Alex"
+
+            binding.apply {
+                invalidateAll()
+            }
+        }
+
+        //binding.tvStudentName.text = s.name
+        //binding.tvStudentID.text = s.id
+
+        // val tvName:TextView = findViewById(R.id.tvStudentName)
+
+        // tvName.text = "Ali"
+    }
+}
